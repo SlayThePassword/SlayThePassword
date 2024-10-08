db48d4bf9d11d83fe2df5487523d80cde9f22d72
Jonathan Cherry <tuo97200@temple.edu>
2024-10-08 02:37:14 -0400

    Initial skeletons
    + User, Conditions, Password impl

---
+++ SlayThePassword/src/PasswordImp.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+
+
+public class PasswordImp {
+    public final int difficulty;
+    public Conditions[] conditions;
+
+
+    public PasswordImp(String conditions[]){
+        difficulty= 0;
+        this.conditions = conditions;
+    }
+}
